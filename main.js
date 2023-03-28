fb86479f27f8be7856dc317cec6d335bebf22534
stefanoreddd <stefanogg95@gmail.com>
2023-03-28 17:27:53 +0200

    index and main created, to be tested.

---
+++ main.js
@@ -0,0 +1,32 @@
+const nome = prompt("Inserisci il tuo nome");
+const cognome = prompt("Inserisci il tuo cognome");
+const colore = prompt("Inserisci il tuo colore preferito");
+const numero1 = prompt("Scegli un numero");
+const numero2 = prompt("Scegli un altro numero");
+const numeropassword = numero1 / numero2
+
+
+
+let passwordWannabeLog = `
+---------- Password Generator ----------
+>Informazioni Generiche
+Nome: ${nome}
+Cognome: ${cognome}
+
+>Informazioni Secondarie
+Colore preferito: ${colore}
+Primo numero: ${numero1}
+Secondo numero: ${numero2}
+
+Password: ${nome + cognome + colore + numeropassword}
+
+----------------------------------------
+`
+
+let passwordWannabe = `
+${nome+cognome+colore+numeropassword}
+`
+
+console.log(passwordWannabe)
+
+document.getElementById("password-generator").innerHTML = passwordWannabe;
